a0195663814da35871692282f1fdb36068921fad
TylerS2000 <tylerschulten@gmail.com>
2025-04-13 18:23:28 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tyler Schulten on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
