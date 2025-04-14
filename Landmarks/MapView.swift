8beb1b8d66fb639a1e76069a8c9559647f5df3bd
TylerS2000 <tylerschulten@gmail.com>
2025-04-14 18:35:16 -0400

    completes turtle rock tutorial

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Tyler Schulten on 4/14/25.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
